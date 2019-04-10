20c2bb6efd3c947cee3e0a26cb74a6ccfc1116d0
jasminelopez <lopez.jasmine094@gmail.com>
2019-04-10 16:33:23 -0700

    Add functional shared implementation for Stack and pass all test

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+
+  someInstance.len = 0;
+  someInstance.pancakes = {};
+
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+
+  return someInstance;
 };
 
-var stackMethods = {};
 
+var stackMethods = {};
+stackMethods.push = function(val) {
+  this.pancakes[this.len] = val;
+  this.len++;
+};
+stackMethods.pop = function() {
+  this.val = this.pancakes[(this.len - 1)];
+  delete this.pancakes[(this.len - 1)];
+  this.len--;
+  return this.val;
+};
+stackMethods.size = function() {
+  if (this.len < 0) {
+    return 0;
+  }
+  return this.len;
+
+};
 
