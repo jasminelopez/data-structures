00cf288fcb6df3bcc5232f7c748f29cd98e5a83b
jasminelopez <lopez.jasmine094@gmail.com>
2019-04-10 15:39:51 -0700

    Should complete and pass all tests for Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,19 +4,31 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   var size = 0;
+  var index = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index++;
     size++;
   };
 
   someInstance.dequeue = function() {
+    var keys = Object.keys(storage);
+    var minKey = Math.min(...keys);
+    var val = storage[minKey];
+    delete storage[minKey];
     size--;
+    return val;
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      return 0;
+    }
     return size;
   };
-
   return someInstance;
 };
+
+
