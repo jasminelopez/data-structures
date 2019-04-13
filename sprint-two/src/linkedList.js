b67c9e8f4c3b6fa8feb7eb6596bd7ac52b7854f3
jasminelopez <lopez.jasmine094@gmail.com>
2019-04-13 09:32:53 -0700

    Clean up code linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -7,26 +7,25 @@ var LinkedList = function() {
 
 
   list.addToTail = function(value) {
-    
   
-    //currently broken fork on the next 
-    var stringIndex = index.toString(); 
-    list[stringIndex] = new Node(value); 
+    list[index] = new Node(value); 
 
     if (lastAdded !== undefined) {
-      lastAdded.next = list[stringIndex]; //last added will point at current node 
+      lastAdded.next = list[index]; //last added will point at current node 
     }
-    // console.log(lastAdded.next);
+
     if (list.head === null) { //handles the 0th iteration with no contents
-      list.head = list[stringIndex];
+      list.head = list[index];
     }
 
     var formerTail = list.tail; 
+
     if (formerTail !== null) { 
-      formerTail.next = list[stringIndex];
+      formerTail.next = list[index];
     }
-    list.tail = list[stringIndex];
-    lastAdded = list[stringIndex]; //lastAdded is defined in the first call
+
+    list.tail = list[index];
+    lastAdded = list[index]; 
     index++;
   };
 
@@ -34,23 +33,37 @@ var LinkedList = function() {
     var formerHeadNode;
     // find the head- if head is pointing to a node (if list is empty)
     //if head is pointing to the first node 
+
     if (list.head !== null) {
       formerHeadNode = list.head; //store the first elements next value in formerHead
-      //current head node is the node we are removing
       var currentHeadNode = formerHeadNode.next; // We are trying to grab what it was pointing to before removing it
       //head is now pointing to the currect head node
-      list.head = currentHeadNode; // reset head
+      list.head = currentHeadNode; 
     }
     return formerHeadNode.value;
   };
 
   list.contains = function(target) {
-    //loop //
- 
+    
+    var checkingNode;
+    if (checkingNode === undefined) {
+      checkingNode = list.head; 
+    }
+    while (list.tail !== checkingNode) {
+      if (checkingNode.value === target) {
+        return true;
+      } else {
+        checkingNode = checkingNode.next;
+      }
+      if (list.tail.value === target) {
+        return true;
+      }
+    }
+    return false; 
   };
   return list;
 };
-//constructor node
+
 var Node = function(value) {
   var node = {};
 
@@ -66,13 +79,3 @@ var Node = function(value) {
  * Complexity: What is the time complexity of the above functions?
  */
 // head => {val:2,pointer:node2} => {val:2,pointer:node3}; => {val:2,pointer:node4}  {val:2,pointer:null} <=tail
-// index = 0;
-// list.tail = null
-// list.head = null
-// {
-//   "0"{
-//     value: 4,
-//     next: 
-//   }
-
-// }
